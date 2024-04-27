ea8bded50eece69773a7242bc929ddfbe504eff1
Facu <facuc4rr@gmail.com>
2024-04-27 14:03:21 -0300

    Login

---
+++ src/login/login.tsx
@@ -0,0 +1,19 @@
+const Login = () => {
+    return (
+        <>
+            <div className="flex flex-col">
+                <div>
+                    <h3>Usuario</h3>
+                    <input></input>
+                </div>
+                <div>
+                    <h3>Password</h3>
+                    <input></input>
+                </div>
+                <button>Login</button>
+            </div>
+        </>
+    )
+}
+
+export default Login
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Login from './login/login.tsx'
+import '@css/index.css'
+
+const routes = [
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  }
+  /*{
+    path: "/game",
+    element: <Game/>
+  }*/
+]
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
