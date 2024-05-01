543c89a1c786ce31d648431a60c4460943318fa0
Facu <facuc4rr@gmail.com>
2024-05-01 11:13:50 -0300

    Icons

---
+++ src/Icons.tsx
@@ -0,0 +1,17 @@
+import { Check } from "./icons/Check"
+import Email from "./icons/Email"
+import Search from "./icons/Search"
+import Password from "./icons/Password"
+import Username from "./icons/Username"
+import Eye from "./icons/Eye"
+import EyeHidden from "./icons/EyeHidden"
+
+export const Icons = {
+    username: <Username/>,
+    password: <Password/>,
+    email: <Email/>,
+    eye: <Eye/>,
+    eyeHidden: <EyeHidden/>,
+    search: <Search/>,
+    check: <Check/>,
+}
