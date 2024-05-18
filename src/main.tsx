6a735995e0d3b103d599e3654ab5ccba5b1e7235
Facu <facuc4rr@gmail.com>
2024-05-18 12:19:11 -0300

    Roled Rules

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,22 @@
+import '@css/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './routes/main/App.tsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import RouteProvider from './context/RouteContext.tsx'
-import Login from './routes/login/login.tsx'
-import '@css/index.css'
-import Maps from './routes/maps/Map.tsx'
+import Login from './routes/login/Login.tsx'
+import AuthProvider from './context/AuthContext.tsx'
+import RoleBasedRouting from './libs/RoleRouting.tsx'
 
 const routes = [
   {
-    path: "/",
-    element: <App/>,// app debe subdividirse en 3: Intercambiador, Ayudante, Administrador
+    path: "/*",
+    element:
+    <AuthProvider>
+      <RoleBasedRouting/>
+    </AuthProvider>,
   },
   {
     path: "/login",
     element: <Login/>
-  },
-  {
-    path: "/maps",
-    element: <Maps/>
   }
 ]
 
@@ -26,8 +24,6 @@ const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouteProvider>
-      <RouterProvider router={router} />
-    </RouteProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
