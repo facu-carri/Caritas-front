2901c790b2c09203f77a6b169e4e89ece2a2b897
Facu <facuc4rr@gmail.com>
2024-05-18 18:27:46 -0300

    Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navbar-blue': '#BACDE3',
+      },
+    }
   },
   plugins: [
     daisyui
