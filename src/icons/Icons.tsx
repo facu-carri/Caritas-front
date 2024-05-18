6b104e004139cf62008e8013103ce6bd82619c84
Facu <facuc4rr@gmail.com>
2024-05-18 12:15:39 -0300

    Relocate

---
+++ src/icons/Icons.tsx
@@ -0,0 +1,17 @@
+import { Check } from "./Check"
+import Email from "./Email"
+import Search from "./Search"
+import Password from "./Password"
+import Username from "./Username"
+import Eye from "./Eye"
+import EyeHidden from "./EyeHidden"
+
+export const Icons = {
+    username: <Username/>,
+    password: <Password/>,
+    email: <Email/>,
+    eye: <Eye/>,
+    eyeHidden: <EyeHidden/>,
+    search: <Search/>,
+    check: <Check/>,
+}
