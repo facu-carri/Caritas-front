733288e32bc150d7a4730dec4402a76b73a3f98c
Facu <facuc4rr@gmail.com>
2024-05-19 12:13:22 -0300

    Navbar key items

--- src/components/admin/Navbar.tsx
+++ src/components/admin/Navbar.tsx
@@ -17,34 +17,34 @@ type NavbarType = {
 
 export default function Navbar({startTabs: start, middleTabs: middle, endTabs: end}: NavbarType) {
 
-    const getTabObjs = (tabs:Array<Tab>) => {
+    const getTabObjs = (tabs:Array<Tab>, key:string) => {
         return tabs && tabs.map((tab, index) => (
-            <>
+            <div key={`${key}_${index}`}>
                 {tab.customElement ? tab.customElement :
-                    <Button text={tab.text} icon={tab.icon} visible={tab.visible} active={tab.visible} onClick={tab.onClick} key={index}/>
+                    <Button text={tab.text} icon={tab.icon} visible={tab.visible} active={tab.visible} onClick={tab.onClick} key={`${key}_${index}`}/>
                 }
-            </>
+            </div>
         ))
     }
 
     return (
         <div className="px-5 fixed top-0 left-0 w-full text-white z-10">
-            <div className="navbar bg-navbar-blue mt-2 shadow-xl rounded-box">
+            <div className="navbar bg-navbar-blue mt-2 shadow-xl rounded-box flex-1">
                 <div className="navbar-start gap-2">
                     {
-                        getTabObjs(start)
+                        getTabObjs(start, 'start')
                     }
                 </div>
                 <div className="navbar-center lg:flex">
                     <ul className="menu menu-horizontal px-1 gap-4">
                         {
-                            getTabObjs(middle)
+                            getTabObjs(middle, 'middle')
                         }
                     </ul>
                 </div>
                 <div className="navbar-end gap-2">
                     {
-                        getTabObjs(end)
+                        getTabObjs(end, 'end')
                     }
                 </div>
             </div>
