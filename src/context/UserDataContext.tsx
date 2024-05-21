35d1c037ab62ddfd0f4ec16f07dd2226cbf771d9
Facu <facuc4rr@gmail.com>
2024-05-21 20:26:11 -0300

    Context changes

---
+++ src/context/UserDataContext.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react-refresh/only-export-components */
+import React, { useContext, useState } from "react";
+import { UserDataContextType } from "./types/User";
+
+const UserDataContext = React.createContext<UserDataContextType>(undefined)
+
+export function useUserData(){
+    return useContext(UserDataContext)
+}
+
+export default function UserProvider({ children }) {
+
+    const [userData, setUserData] = useState(false)
+
+    return (
+        <UserDataContext.Provider value={{ userData, setUserData }}>
+            {children}
+        </UserDataContext.Provider>
+    )
+}
---
+++ src/context/types/User.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type UserDataContextType = {
+    userData: any,
+    setUserData: (data) => void
+}
