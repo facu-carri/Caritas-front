72987674724dcccdd7b9bb61b8fd82e4d3b4e054
LisandroCausa <lisandro.anexa@gmail.com>
2024-05-21 20:32:30 -0300

    feat: show item photos

--- src/components/exchanger/ProductCard.tsx
+++ src/components/exchanger/ProductCard.tsx
@@ -6,7 +6,7 @@ function ProductCard({ product }) {
   return (
     <div className="bg-white p-4 rounded shadow-lg">
       <div className="mb-4">
-        <img src={photo} alt={name} className="w-full h-auto rounded" />
+        <img src={`data:image/jpeg;base64,${photo}`} alt={name} className="w-full rounded" />
       </div>
       <h2 className="text-xl font-bold mb-2">{name}</h2>
       <p className="text-sm text-gray-500 mb-2">ItemCategory: {itemCategory.name}</p>
