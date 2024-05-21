be44af9fa4d7b306aa41968fcdef8264208b7043
Facu <facuc4rr@gmail.com>
2024-05-20 23:03:20 -0300

    Add space

--- src/components/helper/HelpersManager.tsx
+++ src/components/helper/HelpersManager.tsx
@@ -39,7 +39,7 @@ export default function HelpersManager() {
     <dialog className="modal" id='registerModal'>
         <RegisterHelper modalId={'registerModal'} />
     </dialog>
-    <div className="min-h-screen bg-gray-100 flex items-center flex-col gap-4 justify-center p-4">
+      <div className="min-h-screen bg-gray-100 flex items-center flex-col gap-4 justify-center p-4">
       <Button onClick={handleRegisterHelper} >Registrar ayudante</Button>
       <div className="w-full max-w-2xl bg-white shadow-lg rounded-lg p-6">
         <h1 className="text-2xl font-bold text-blue-700 mb-4">Listado de Ayudantes</h1>
