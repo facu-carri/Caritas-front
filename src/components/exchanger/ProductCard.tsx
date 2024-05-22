c833208cec9a3a30032a3f5297bb10bca9b7a09e
Facu <facuc4rr@gmail.com>
2024-05-22 03:51:25 -0300

    ProductCard visual change

--- src/components/exchanger/ProductCard.tsx
+++ src/components/exchanger/ProductCard.tsx
@@ -11,16 +11,18 @@ function ProductCard({ product }) {
   }
   return (
     <div className="bg-white p-4 rounded shadow-lg">
-      <div className="mb-4">
-        <img src={`data:image/jpeg;base64,${photo}`} alt={name} className="w-full rounded" />
-      </div>
-      <h2 className="text-xl font-bold mb-2">{name}</h2>
-      <p className="text-sm text-gray-500 mb-2">ItemCategory: {itemCategory.name}</p>
-      <p className="text-sm text-gray-500 mb-2">Description: {description}</p>
-      <p className="text-sm text-gray-500 mb-2">Cantidad restante: {quantity}</p>
-      <button className="bg-red-500 text-white px-4 py-2 rounded">Intercambiar </button>
-      <button onClick={onClickHandler} className="bg-blue-500 text-white px-4 py-2 mt-2 rounded">Dueño del item: {owner.name}</button>
+    <div className="mb-4">
+      <img src={`data:image/jpeg;base64,${photo}`} alt={name} className="w-full rounded" />
     </div>
+    <h2 className="text-xl font-bold mb-2">{name}</h2>
+    <p className="text-sm text-gray-500 mb-2">ItemCategory: {itemCategory.name}</p>
+    <p className="text-sm text-gray-500 mb-2">Description: {description}</p>
+    <p className="text-sm text-gray-500 mb-2">Cantidad restante: {quantity}</p>
+    <div className="flex flex-col items-start">
+      <button className="bg-red-500 text-white px-4 py-2 rounded mb-2">Intercambiar</button>
+      <button onClick={onClickHandler} className="bg-blue-500 text-white px-4 py-2 rounded">Dueño del item: {owner.name}</button>
+    </div>
+  </div>
   );
 }
 
