9a6b3845d5897f478fd0cd580de9bd520e681ab4
Facu <facuc4rr@gmail.com>
2024-05-22 00:15:25 -0300

    Fix navbar helper

--- src/routes/helper/main.tsx
+++ src/routes/helper/main.tsx
@@ -1,12 +1,7 @@
-import Navbar from "src/components/admin/Navbar"
 
 export const HelperMain = () => {
   return (
     <>
-      <Navbar></Navbar>
-      <h1 className="flex justify-center items-center h-[100vh] text-[100%]">
-      USUARIO HELPER
-    </h1>
     </>
   )
 }
