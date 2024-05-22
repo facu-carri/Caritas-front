998e277fc1a859fc1ce122df7e6de1c9583658cc
LisandroCausa <lisandro.anexa@gmail.com>
2024-05-22 12:12:13 -0300

    style: employee profile button

--- src/components/helper/HelperItem.tsx
+++ src/components/helper/HelperItem.tsx
@@ -5,9 +5,11 @@ const HelperItem = ({ helper, onEdit, onDelete, onSelect }) => {
   return (
     <li className="flex items-center justify-between p-4 border-b border-gray-200">
       <div className="flex items-center">
-        <a onClick={() => onSelect(helper.id)}>
-          <FaUser className="text-blue-500 mr-2" />
-        </a>
+        <button
+          onClick={() => onSelect(helper.id)}
+        >
+          <FaUser className="text-blue-500 mr-2 hover:text-blue-700 transition duration-300" />
+        </button>
         <span className="text-lg text-gray-700">{`${helper.name} ${helper.email}`}</span>
       </div>
       <div className="flex space-x-2">
