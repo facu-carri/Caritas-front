b757219170238ac95c128059d9e5c720755f7afc
Facu <facuc4rr@gmail.com>
2024-05-25 14:50:23 -0300

    Syntax change

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-
     </QueryClientProvider>
   </React.StrictMode>
 )
