e4581b1069b756dc51f9c9a5fcd3a9fdd9df8af3
Facu <facuc4rr@gmail.com>
2024-05-27 01:17:28 -0300

    Remove simplify code

--- src/routes/exchanger/pages/Information.tsx
+++ src/routes/exchanger/pages/Information.tsx
@@ -4,11 +4,9 @@ export const Information = () => {
   return (
     <div className="flex flex-col items-center justify-start min-h-screen bg-gray-900 pt-20">
       <div className="max-w-3xl px-6 py-12 text-center">
-        <div className="mb-8">
-          <h1 className="mt-4 text-4xl font-serif font-bold tracking-tight text-gray-100">
-            Caritas
-          </h1>
-        </div>
+        <h1 className="mb-8 mt-4 text-4xl font-serif font-bold tracking-tight text-gray-100">
+          Caritas
+        </h1>
         <p className="mx-auto mb-8 w-full text-lg font-medium leading-relaxed text-gray-400">
           En Caritas Internationalist, entendemos que muchas personas tienen bienes o servicios que desean intercambiar, pero a menudo se enfrentan a diversas barreras.
           Para superar estos obstáculos, hemos desarrollado una plataforma innovadora que facilita el intercambio de bienes y servicios de manera segura y eficiente.
