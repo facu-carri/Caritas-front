b729d61ffd6ae9058b55d68f48ef5bb8d6e64e03
LisandroCausa <lisandro.anexa@gmail.com>
2024-05-30 21:27:38 -0300

    update: register error messages

--- src/utils/Error/ErrorMessages/RegisterExchanger.ts
+++ src/utils/Error/ErrorMessages/RegisterExchanger.ts
@@ -2,8 +2,11 @@ import { ErrorTypes } from "../ErrorTypes";
 
 export const RegisterExchangerErrorMsgs = {
     [ErrorTypes.REGISTER_EXCHANGER_ERROR]: {
-        400: 'Consetraña invalida',
-        406: 'Edad invalida',
-        409: 'Mail ya registrado'
+        400: 'La contraseña debe ser alfanumérica con una longitud mayor o igual a 6 caracteres',
+        403: 'Todos los campos son obligatorios',
+        406: 'La persona debe ser mayor de edad',
+        409: 'Email ya registrado',
+        // TODO: cambiar status code correspondiente
+        //: 'El email pertenece a una cuenta suspendida por infligir las normas de la comunidad' 
     }
 }
