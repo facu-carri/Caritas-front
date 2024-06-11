e78cf9f79553226d3955ba57cca4192b07f2c788
Facu <facuc4rr@gmail.com>
2024-06-11 07:29:10 -0300

    Add register helper code errors

--- src/utils/Error/ErrorMessages/RegisterHelper.ts
+++ src/utils/Error/ErrorMessages/RegisterHelper.ts
@@ -2,7 +2,10 @@ import { ErrorTypes } from "../ErrorTypes";
 
 export const RegisterHelperErrorMsgs = {
     [ErrorTypes.REGISTER_HELPER_ERROR]: {
-        401: 'Only admins',
-        404: 'Campos invalidos'
+        400: "Contraseña invalida",
+        401: "Solo administradores pueden registrar ayudantes",
+        406: "La fecha es de un menor de edad",
+        409: "Email ya registrado",
+        451: "Email invalido"
     }
 }
