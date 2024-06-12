0bdf64ca3802e395320d6a4f575aea19e3503a27
LisandroCausa <lisandro.anexa@gmail.com>
2024-06-12 01:14:53 -0300

    add: more info to AcceptNotificationModal

--- src/routes/exchanger/components/inventory/AcceptNotificationModal.tsx
+++ src/routes/exchanger/components/inventory/AcceptNotificationModal.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
+import { FormField } from "src/types/PropsTypes";
 import { getData, putData } from "src/utils/request/httpRequests";
-import { endPoints } from "src/utils/constants";
-import GenericForm from "src/components/GenericForm";
+import { endPoints, routes } from "src/utils/constants";
 import { ErrorTypes } from "src/utils/Error/ErrorTypes";
 import { ErrorCode } from "src/utils/Error/ErrorCode";
+import GenericForm from "src/components/GenericForm";
 import { selectLocations } from "src/components/modals/modalOptions";
-import { FormField } from "src/types/PropsTypes";
+import Image from "src/components/Image";
 
-export default function EditItemModal({ notificationData, onEditNotification }) {
+export default function AcceptNotificationModal({ notificationData, onEditNotification }) {
 
   const [freeLocations, setFreeLocations] = useState([]);
   const [nextFreeDay, setNextFreeDay] = useState("");
@@ -18,10 +19,10 @@ export default function EditItemModal({ notificationData, onEditNotification }) 
   const handleError = (errCode: number) => {
     const err = new ErrorCode(errCode, ErrorTypes.EDIT_ITEM_ERROR)
     setError(err)
-    setTimeout(hiddeError, 5000)
+    setTimeout(hideError, 5000)
   }
 
-  const hiddeError = () => setError(null)
+  const hideError = () => setError(null)
 
   const handleAcceptNotification = (data) => {
     data.date=nextFreeDay
@@ -35,8 +36,7 @@ export default function EditItemModal({ notificationData, onEditNotification }) 
   }
 
   function getDefaultFileds(): Array<FormField> {
-    return [
-    ]
+    return []
   }
 
   const handleGetNextDay = (data:string) => {
@@ -60,24 +60,38 @@ export default function EditItemModal({ notificationData, onEditNotification }) 
   return (
     campos && campos.length > 0 && (
       <div>
-        <GenericForm id="register-helper" campos={campos} listener={handleAcceptNotification} error={error}>
+        <GenericForm id="register-helper" campos={campos} listener={handleAcceptNotification} error={error} hideImg={true}>
+          <>
           {[
-            { label: "Solicitante", value: notificationData.hostItem?.owner?.name },
-            { label: "Item del solicitante", value: notificationData.hostItem?.name },
-            { label: "Tu item", value: notificationData.guestItem?.name },
+            { label: "Solicitante", value: notificationData.hostItem?.owner?.name, href: `${routes.exchanger.profile}/${notificationData.hostItem.owner.id}` },
+            { label: "Item del solicitante", value: notificationData.hostItem?.name, photo: notificationData.hostItem?.photo  },
+            { label: "Tu item", value: notificationData.guestItem?.name, photo: notificationData.guestItem?.photo },
             { label: "Cantidad restante de tu item", value: notificationData.guestItem?.quantity },
             { label: "Categoria", value: notificationData.guestItem?.itemCategory?.name },
-            { label: "La fecha sera", value: nextFreeDay },
+            { label: "La fecha será", value: nextFreeDay },
           ].map((item) => (
             <div key={item.label} className="flex items-center mb-2">
               <span key={item.label} className="text-sm font-bold text-gray-500 mr-2">
                 {item.label}:
               </span>
-              <span className="text-sm font-bold text-gray-500 mr-2">
-                {item.value}
+              <span className={`text-sm font-bold text-gray-500 mr-2 ${item.href ? 'link' : ''}`}>
+                {
+                  item.href ?
+                  <a href={item.href}>
+                    {item.value}
+                  </a>
+                  :
+                  <>
+                    {item.value}
+                  </>
+                }
+              </span>
+              <span>
+                <Image photo={item.photo} className="w-16 h-16" />
               </span>
             </div>
           ))}
+          </>
         </GenericForm>
       </div>
     )
