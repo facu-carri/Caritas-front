3577afd8541899fafde6130aaa180648c21dd8c6
Nack34 <gmnack9@gmail.com>
2024-06-12 11:32:16 -0300

    Update BellNotificationCard.tsx

--- src/routes/exchanger/components/BellNotificationCard.tsx
+++ src/routes/exchanger/components/BellNotificationCard.tsx
@@ -1,13 +1,13 @@
 import { useMutation } from 'react-query';
-import { getHeaders } from 'src/utils/request/httpRequests';
+import { getData, getHeaders } from 'src/utils/request/httpRequests';
 import { useCustomModal } from 'src/context/CustomModalContext';
 import { endPoints, routes, serverAddress } from 'src/utils/constants';
 import RoutesHandler from 'src/utils/routesHandler';
+import { useEffect, useState } from "react";
 
 import { AiOutlineCheck, AiOutlineClose  } from "react-icons/ai";
 import AcceptNotificationModal from './inventory/AcceptNotificationModal';
 import ConfirmationModal from 'src/components/modals/Confirmation';
-import { useState } from 'react';
 
 export default function BellNotificationCard({ notification }) {
   
@@ -45,6 +45,13 @@ export default function BellNotificationCard({ notification }) {
     }
   })
 
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    getData(endPoints.location)
+    .then(data => setLocations(data))
+  }, [])
+
   return (
     <div className="bg-white p-4 rounded shadow-lg max-w-sm cursor-pointer transform transition-transform duration-200 hover:scale-105">
       <button onClick={onClickOwner} className={`bg-red-500 text-white px-4 py-2 rounded transform transition-transform duration-200 hover:scale-105`}>
@@ -57,7 +64,7 @@ export default function BellNotificationCard({ notification }) {
         <div className='flex items-center justify-between'>
         {
           <div className='flex gap-2'>
-            <button onClick={onClickAccept} className='btn btn-error'><AiOutlineCheck /></button>
+            <button onClick={onClickAccept} disabled={!locations || locations.length == 0} className='btn btn-error'><AiOutlineCheck /></button>
             <button onClick={() => confirmation(rejectNotification)} className='btn btn-error'><AiOutlineClose /></button>
           </div>
         }
