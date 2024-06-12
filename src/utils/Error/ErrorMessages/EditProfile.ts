bccbdb8c6dcf3447da40c1e7a53522a9b3e0c4e0
Nack34 <gmnack9@gmail.com>
2024-06-11 21:32:19 -0300

    .

--- src/utils/Error/ErrorMessages/EditProfile.ts
+++ src/utils/Error/ErrorMessages/EditProfile.ts
@@ -2,9 +2,9 @@ import { ErrorTypes } from "../ErrorTypes";
 
 export const EditProfileErrorMsgs = {
     [ErrorTypes.EDIT_PROFILE_ERROR]: {
-        400: 'Contraseña inválida',
+        400: 'La contraseña debe ser alfanumérica mayor o igual a 6 dígitos',
         401: 'Acceso no autorizado',
         404: 'No existe el intercambiador',
-        406: 'La fecha de nacimiento debe ser mayor a 18 años',
+        406: 'La fecha de nacimiento debe pertenecer a una persona mayor de edad',
     }
 }
--- src/utils/Error/ErrorMessages/RegisterHelper.ts
+++ src/utils/Error/ErrorMessages/RegisterHelper.ts
@@ -2,9 +2,9 @@ import { ErrorTypes } from "../ErrorTypes";
 
 export const RegisterHelperErrorMsgs = {
     [ErrorTypes.REGISTER_HELPER_ERROR]: {
-        400: "Contraseña invalida",
+        400: "La contraseña debe ser alfanumérica mayor o igual a 6 dígitos",
         401: "Solo administradores pueden registrar ayudantes",
-        406: "La fecha es de un menor de edad",
+        406: "La fecha de nacimiento debe pertenecer a una persona mayor de edad",
         409: "Email ya registrado",
         451: "Email invalido"
     }
