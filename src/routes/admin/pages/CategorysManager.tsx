e8525da50e4d07d3cb4f4781501ed4dd7b5550c3
Nack34 <gmnack9@gmail.com>
2024-06-28 17:02:03 -0300

    .

--- src/routes/admin/pages/CategorysManager.tsx
+++ src/routes/admin/pages/CategorysManager.tsx
@@ -6,6 +6,9 @@ import ExchangerHeader from 'src/components/ExchangerHeader';
 import { useQueryClient } from 'react-query';
 import AddCategoryModal from "../components/AddCategoryModal";
 import CategorysList from "../components/CategorysList";
+import { useState } from "react";
+import { ErrorCode } from "src/utils/Error/ErrorCode";
+import { ErrorTypes } from "src/utils/Error/ErrorTypes";
 
 export default function CategorysManager() {
     const { showModal, closeModal } = useCustomModal()
@@ -13,7 +16,16 @@ export default function CategorysManager() {
     const ruta = endPoints.inventory
 
     const queryClient = useQueryClient() // Y esto que es?
+    const [error, setError] = useState<ErrorCode>(null)
 
+    const handleError = (errCode: number) => {
+        const err = new ErrorCode(errCode, ErrorTypes.ITEMCATEGORYS_ERROR)
+        setError(err)
+        setTimeout(hiddeError, 5000)
+    }
+    const hiddeError = () => {
+        setError(null)
+    }
     const handleModal = () => showModal(<AddCategoryModal onClose={closeModal} onAddItemCategory={addItemCategory}/>)
     const addItemCategory = (category) => 
         postData(endPoints.itemCategories, null, category)
@@ -21,15 +33,18 @@ export default function CategorysManager() {
                 queryClient.invalidateQueries([ruta]); // Y esto que es?
                 queryClient.invalidateQueries(['inventory']); // Y esto que es?
             })
+            .catch((errCode: number) => handleError(errCode))
+            
     
     return (
         <div className="bg-gray-100 min-h-screen">
-            <ExchangerHeader title="Categorias" text="Aviso: Si elimina una categoria se eliminaran todas las publicaciones que tengan esta categoria asignada. Le recomendamos nunca eliminar una categoria"/>
+            <ExchangerHeader title="Categorias" text="Aviso: Si elimina una categoria se eliminaran todas las publicaciones que tengan esta categoria asignada. Le recomendamos nunca eliminar una categoria. Una categoria eliminada no se podra volver a cargar."/>
             <CategorysList >
                 <button onClick={handleModal} className="p-2 flex flex-row items-center space-x-2 px-2 gap-2 text-sm font-medium text-white bg-red-600 rounded hover:bg-red-500">
                     <BsPlus className='h-4 w-4'/>
                     Agregar Categoria
                 </button>
+                {error && <p>La categoria ingresada ya existe o existio y fue eliminada. </p>}
             </CategorysList>
         </div>
     )
