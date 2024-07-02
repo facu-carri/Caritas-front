ca20b54d6f188667767ab78f1d38d8aa183c2419
Nack34 <gmnack9@gmail.com>
2024-07-02 18:20:06 -0300

    Update DonationCard.tsx

--- src/routes/admin/components/DonationCard.tsx
+++ src/routes/admin/components/DonationCard.tsx
@@ -10,14 +10,14 @@ import ExchangerCard from "./ExchangerCard";
 export default function DonationCard({ cardData }) {
 
     return (
-        <div className='relative bg-white rounded-lg shadow-lg max-w-sm p-2 cursor-pointer transform transition-transform duration-200 hover:scale-10' >
+        <div className='relative h-fit bg-white rounded-lg shadow-lg max-w-sm p-2 cursor-pointer transform transition-transform duration-200 hover:scale-10' >
             <div className="px-2 py-1">
-                {
+                {/*
                     cardData.donor? 
                         <ExchangerCard cardData={cardData.donor} key={cardData.donor.id} />
                     :   
                         <p>Esta donacion ha sido anonima</p>
-                }
+                */}
             </div>
             {cardData.amount && <InformativeText icon={<FaBitcoin />}>{cardData.amount}</InformativeText>}
             {cardData.date && <InformativeText icon={<BsCalendar />}>{cardData.date}</InformativeText>}
